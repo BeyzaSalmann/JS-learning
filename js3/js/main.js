0b7138244f94c78e1141fbe451c7ee6f974fe0e3
BeyzaSalmann <beyzasalman02@gmail.com>
2026-02-09 14:34:23 +0300

    Chapter 3: Strings

---
+++ js3/js/main.js
@@ -0,0 +1,24 @@
+//Strings
+const myVariable = "Mathematics";
+
+//The length property
+console.log("Every good boy does fine".length);
+
+//String methods
+console.log(myVariable.charAt(5));
+console.log(myVariable.indexOf("mat"));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(5, 8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("div"));
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
+console.log("John,Joe,Dave".split(","));
+console.log("Every good boy does fine".split(" "));
+
+
+
+
+
+
