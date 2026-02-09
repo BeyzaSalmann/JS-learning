62fe468d4b9fc6b63f2c078b407213bc6633db31
BeyzaSalmann <beyzasalman02@gmail.com>
2026-02-09 10:09:08 +0300

    Chapter 1: Basic Syntax and Variables

---
+++ js1/main.js
@@ -0,0 +1,47 @@
+//this is a comment
+
+//data types
+typeof "Dave" //string
+typeof 42 //number
+typeof true //boolean
+typeof false //boolean
+
+//variables
+let myVariable ;
+
+typeof myVariable //undefined
+myVariable = "Dave" ; //Dave
+typeof myVariable //string
+myVariable = 42 ; //42
+typeof myVariable //number
+
+//mathematical operators
+42+2; //44
+42-2; //40
+let myNumber = 42 ;//underfined
+myNumber + 2; //44
+myNumber - 2; //40
+myNumber * 2; //84
+myNumber / 2; //21
+myNumber = 44*2; //88
+myNumber; //88
+
+myVariable; //42
+myVariable ="Dave"; //Dave
+myNumber + myVariable; //88Dave
+let myCombo = myNumber + myVariable; //underfined
+typeof myCombo; //string
+myCombo; //88Dave
+myNumber + 2; //90
+
+myVariable ===myNumber; //false
+myNumber; //88
+myNumber === 88; //true
+let myBool = myNumber ===88 ;//underfined
+myBool;//true
+myNumber !==88; //false
+myNumber >= 88; //true
+myNumber < 88; //false
+
+alert("Hello World"); //pop up alert box with "Hello World"
+
