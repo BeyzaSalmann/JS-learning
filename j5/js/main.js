b9cfee41a1c4a2e627f13211d288c68b6d5ab89f
BeyzaSalmann <beyzasalman02@gmail.com>
2026-02-09 15:36:50 +0300

    Chapter 5:Math Methods

---
+++ j5/js/main.js
@@ -0,0 +1,15 @@
+// Math Methods and Properties
+
+console.log(Math.PI);//Returns the value of PI
+console.log(Math.trunc(Math.PI)); // Returns the integer part of PI
+console.log(Math.round(3.5));// Rounds the number to the nearest integer
+console.log(Math.ceil(3.2)); // Rounds the number up to the nearest integer
+console.log(Math.floor(3.8)); // Rounds the number down to the nearest integer
+
+//Math Methods and Properties
+console.log(Math.pow(2,4));// Calculates 2 to the power of 4
+console.log(Math.min(2,0.5,9));// Returns the smallest of the given numbers
+console.log(Math.max(2,0.5,9));// Returns the largest of the given numbers
+console.log(Math.random());// Generates a random number between 0 and 1
+//From 1 to 10
+console.log(Math.floor(Math.random() * 10 + 1));//1-10 random number generator
