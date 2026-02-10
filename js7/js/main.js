4698ccfee09516b4b75ce00c15da5b08d5e0aeeb
BeyzaSalmann <beyzasalman02@gmail.com>
2026-02-10 09:34:44 +0300

    Chapter 7: Switch Statements

--- js7/js/main.js
+++ js7/js/main.js
@@ -1,78 +1,59 @@
-//Conditionals: If Statements
+//Conditionals: Switch Statements
 
-//sytntax
-if (condition) {
-    //run some code
-}
-else{
-    //run some diffrent code
+//sytax
+switch (expression OR value) {
+
+    case choice1:
+        //run this code
+        break;
+
+    case choice2:
+        //run this different code
+        break;
+
+    // add as many cases as needed
+
+    default:
+        //run this code if no cases matches
+        //no need for break statement here
 }
 
 //Example
-let customerIsBanned = false;
-let soup="chicken noodle soup";
-let crakers =true;
-let reply;
-if(customerIsBanned){
-    reply = "No soup for you!";
+
+switch  (2) {
+
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+       console.log(2);
+        break;
+
+    case 3:
+       console.log(3);
+        break;
+
+    default:
+        console.log("No match"); 
 }
-else if(soup && crakers){
-    reply = `Here's your order of ${soup} with crackers`;
-}
-else if(soup){
-    reply = `Here's your order of ${soup}`;
-}
-else{
-    reply = `Sorry, we're out of soup ${soup}`;
-}
-console.log(reply);
 
 //Example 2
-let testScore = 89;
-let grade;
 
-if(testScore >= 90){
-    grade = "A";
-}
-else if(testScore >= 80){
-    grade = "B";
-}
-else if(testScore >= 70){
-    grade = "C";
-}
-else if(testScore >= 60){
-    grade = "D";
-}
-else{
-    if(collegeStudent){
-        grade = "U";
-    } else{
-    grade = "F";
-}
-}
-console.log(grade);
+switch  (Math.floor) {
 
-//Example 3 = Decision tree
-if (playerOne === computer){
-    //tie game
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+       console.log(2);
+        break;
+
+    case 3:
+       console.log(3);
+        break;
+
+    default:
+        console.log("No match"); 
 }
-else if(playerOne === "rock"){
-    if(computer === "paper"){
-        //computer wins
-    }
-    else{
-        //player one wins
-    }
-}else if(playerOne === "paper"){
-    if(computer === "scissors"){
-        //computer wins
-    }
-    else{
-        //player one wins
-    }
-}else 
-    if(computer === "rock"){
-        //computer wins 
-    }else {
-        //player one wins   
-    }
